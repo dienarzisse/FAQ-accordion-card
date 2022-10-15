d18dae5726dddf7b6b18a8909ad94c24b6826a66
die_narzisse <bulainarcis7@gmail.com>
2022-10-15 16:04:02 +0200

    second try

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(panelNumber){
     function accordion(panelNumber){
         var rotateDegrees = 0;
         $(`#panel-accordion${panelNumber}`).toggle();
-        $(`#accordion${panelNumber} h3`).on( "click", function(){
+        $(`#accordion${panelNumber}`).on( "click", function(){
            $(`#panel-accordion${panelNumber}`).toggle();
            rotateDegrees += 180;
            $(`#accordion${panelNumber} img`).css("transform",
